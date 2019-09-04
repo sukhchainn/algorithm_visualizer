e1f0f36ecc5f30923dac1ba0f496450412c241b1
Brain Developer <ssaulakh2@gmail.com>
2019-09-04 13:29:52 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_font.h>
+
+#define WIDTH 1000
+#define HEIGHT 600
+
+int main()
+{
+    if(!al_init())
+    {
+        printf("couldn't initialize allegro!\n");
+        return 1;
+    }
+
+    al_init_font_addon();
+
+    if(!al_install_keyboard())
+    {
+        printf("couldn't initialize keyboard!\n");
+        return 1;
+    }
+
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
+    if(!timer)
+    {
+        printf("couldn't initialize timer!\n");
+        return 1;
+    }
+
+    ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
+    if(!queue)
+    {
+        printf("couldn't initialize queue!\n");
+        return 1;
+    }
+
+    ALLEGRO_DISPLAY* disp = al_create_display(WIDTH, HEIGHT);
+    if(!disp)
+    {
+        printf("couldn't initialize display!\n");
+        return 1;
+    }
+
+    ALLEGRO_FONT* font = al_load_font("font.ttf", 5, 0);
+    if(!font)
+    {
+        printf("couldn't initialize font!\n");
+        //return 1;
+    }
+
+    al_register_event_source(queue, al_get_keyboard_event_source());
+    al_register_event_source(queue, al_get_display_event_source(disp));
+    al_register_event_source(queue, al_get_timer_event_source(timer));
+
+    bool done = false;
+    bool redraw = true;
+    ALLEGRO_EVENT event;
+
+    al_start_timer(timer);
+    while(1)
+    {
+        al_wait_for_event(queue, &event);
+
+        switch(event.type)
+        {
+            case ALLEGRO_EVENT_TIMER:
+                // game logic goes here.
+                redraw = true;
+                break;
+
+            //case ALLEGRO_EVENT_KEY_DOWN:
+            case ALLEGRO_EVENT_DISPLAY_CLOSE:
+                done = true;
+                break;
+        }
+
+        if(done)
+            break;
+
+        if(redraw && al_is_event_queue_empty(queue))
+        {
+            al_clear_to_color(al_map_rgb(70, 70, 70));
+            al_draw_text(font, al_map_rgb(255, 255, 255), 0, 0, 0, "Hello world!");
+            al_flip_display();
+
+            redraw = false;
+        }
+    }
+
+    al_destroy_font(font);
+    al_destroy_display(disp);
+    al_destroy_timer(timer);
+    al_destroy_event_queue(queue);
+
+    return 0;
+}
+
+
